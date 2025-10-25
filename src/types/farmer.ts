cba36373d954e0499306a0b839bca3e2b98c8a4f
eduardo <eduardomendes418@gmail.com>
2025-10-25 17:15:25 -0300

    [BAG-18]feat - create types

---
+++ src/types/farmer.ts
@@ -0,0 +1,44 @@
+export interface Farmer {
+  id: string;
+  document: string;
+  name: string;
+  farms: Farm[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Farm {
+  id: string;
+  name: string;
+  city: string;
+  state: string;
+  totalArea: number;
+  agriculturalArea: number;
+  vegetationArea: number;
+  crops: Crop[];
+  farmerId: string;
+}
+
+export interface Crop {
+  id: string;
+  name: string;
+  harvest: string;
+  plantedArea: number;
+  farmId: string;
+}
+
+export interface DashboardData {
+  totalFarms: number;
+  totalHectares: number;
+  farmsByState: ChartData[];
+  farmsByCrop: ChartData[];
+  landUsage: ChartData[];
+}
+
+export interface ChartData {
+  label: string;
+  value: number;
+  percentage: number;
+}
+
+
