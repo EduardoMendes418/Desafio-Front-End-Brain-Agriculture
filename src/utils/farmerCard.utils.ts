e737f4a3fa5cb60d528a59b19f33c2d276701a91
eduardo <eduardomendes418@gmail.com>
2025-10-25 17:15:39 -0300

    [BAG-19]feat - create utis

---
+++ src/utils/farmerCard.utils.ts
@@ -0,0 +1,21 @@
+import type { Farmer } from "../types/farmer";
+
+export const formatArea = (area: number): string => {
+  return `${area.toLocaleString("pt-BR")} ha`;
+};
+
+export const calculateTotalFarmsArea = (farms: Farmer["farms"]): number => {
+  return farms.reduce((sum, farm) => sum + farm.totalArea, 0);
+};
+
+export const getTotalCrops = (farms: Farmer["farms"]): number => {
+  return farms.reduce((sum, farm) => sum + farm.crops.length, 0);
+};
+
+export const calculateAvailableArea = (farm: {
+  totalArea: number;
+  agriculturalArea: number;
+  vegetationArea: number;
+}): number => {
+  return farm.totalArea - farm.agriculturalArea - farm.vegetationArea;
+};
